e3e974809712cb3898d97450cf5dd137b9324d2f
wajuart <wajuart7@gmail.com>
2020-04-11 23:24:04 +0900

    ルーティングの設定／コントローラーの設定

---
+++ test/controllers/registers_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class RegistersControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get registers_index_url
+    assert_response :success
+  end
+
+end
