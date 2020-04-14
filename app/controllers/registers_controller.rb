ac0145c9df1bcac7a1da80c86837becc519a36f5
wajuart <wajuart7@gmail.com>
2020-04-15 00:03:10 +0900

    コンテンツ登録btnリンク設定

--- app/controllers/registers_controller.rb
+++ app/controllers/registers_controller.rb
@@ -1,12 +1,23 @@
 class RegistersController < ApplicationController
-  
+  before_action :set_user
+
   def index
+    # @register = Register.new
+    # @registers = @user.registers.includes(:user)
   end
 
   def new
   end  
 
   def create
+    @register = @user.registers.new(register_params)
+    if @register.save
+      redirect_to user_registers_path(@user), notice: '本が登録されました。'
+    else
+      @registers = @user.registers.includes(:user)
+      flash.now[:alert] = '本のタイトルまたは画像を入力してください。'
+      render :index
+    end
   end
 
   def show
@@ -21,5 +32,14 @@ class RegistersController < ApplicationController
   def delete
   end
 
+  private
+
+  def register_params
+    params.require(:register).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_user
+    # @user = User.find(params[:user_id])
+  end
 
 end
