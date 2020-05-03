17fe9bb130a89dfcc602264146854a578b6c0834
wajuart <wajuart7@gmail.com>
2020-05-03 10:17:41 +0900

    ヘッダー／フッター修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users  
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+                                      sessions: 'users/sessions' } 
   # get 'home/index'
   # root 'books#edit'
 
